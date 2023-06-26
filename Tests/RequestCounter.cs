a7b6ef004030961dff03e1b31ce0bf69f90e7ec1
Maarten van Keulen <mcmaartenz@outlook.com>
2023-06-26 23:35:16 +0200

    XUnit test gdpr refused/unset

--- Tests/RequestCounter.cs
+++ Tests/RequestCounter.cs
@@ -7,6 +7,34 @@ namespace Tests
 {
 	public class RequestCounter
 	{
+		[Theory]
+		[InlineData("refuse")]
+		[InlineData("")]
+		[InlineData((string)null)]
+		public async void NotTrackedIfGdprRefused(string gdprString)
+		{
+			Mock<HttpContext> mockHttpContext = new();
+			mockHttpContext.SetupGet(x => x.Request.Cookies["gdpr"]).Returns(gdprString);
+			mockHttpContext.SetupGet(x => x.Request.Cookies["requestCount"]).Returns((string)null);
+
+			HttpContext? capturedContext = null;
+
+			Task requestDelegate(HttpContext ctx)
+			{
+				capturedContext = ctx;
+				return Task.CompletedTask;
+			}
+
+			Mock<IResponseCookies> mockResponseCookies = new();
+			mockHttpContext.SetupGet(x => x.Response.Cookies).Returns(mockResponseCookies.Object);
+
+			RequestCountMiddleware requestCount = new(requestDelegate);
+			await requestCount.InvokeAsync(mockHttpContext.Object);
+
+			Assert.NotNull(capturedContext);
+			Assert.Empty(mockResponseCookies.Invocations);
+		}
+
 		[Fact]
 		public async void IfNoneProvidesOne()
 		{
