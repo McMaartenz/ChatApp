fb1657ef20d9efbf8c2401d02629ecfe6908672a
Maarten van Keulen <mcmaartenz@outlook.com>
2023-06-26 23:31:46 +0200

    XUnit setup

---
+++ Tests/RequestCounter.cs
@@ -0,0 +1,41 @@
+using ChatApp.Middlewares;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Xunit;
+
+namespace Tests
+{
+	public class RequestCounter
+	{
+		[Fact]
+		public async void IfNoneProvidesOne()
+		{
+			Mock<HttpContext> mockHttpContext = new();
+			mockHttpContext.SetupGet(x => x.Request.Cookies["gdpr"]).Returns("accept");
+			mockHttpContext.SetupGet(x => x.Request.Cookies["requestCount"]).Returns((string)null);
+			
+			HttpContext? capturedContext = null;
+
+			Task requestDelegate(HttpContext ctx)
+			{
+				capturedContext = ctx;
+				return Task.CompletedTask;
+			}
+
+			Mock<IResponseCookies> mockResponseCookies = new();
+			mockHttpContext.SetupGet(x => x.Response.Cookies).Returns(mockResponseCookies.Object);
+
+			RequestCountMiddleware requestCount = new(requestDelegate);
+			await requestCount.InvokeAsync(mockHttpContext.Object);
+
+			Assert.NotNull(capturedContext);
+
+			Assert.NotEmpty(mockResponseCookies.Invocations);
+			IInvocation appendInvocation = mockResponseCookies.Invocations[0];
+
+			Assert.Equal("Append", appendInvocation.Method.Name);
+			Assert.Equal("requestCount", appendInvocation.Arguments[0]);
+			Assert.Equal("1", appendInvocation.Arguments[1]);
+		}
+	}
+}
