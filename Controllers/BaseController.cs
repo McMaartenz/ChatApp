46b451746d001cca8a9dad649ea69ff39515291a
Maarten van Keulen <mcmaartenz@outlook.com>
2023-06-27 11:56:39 +0200

    Bugfix: Request count not showing on contact page

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -13,16 +13,27 @@ namespace ChatApp.Controllers
             _logger = logger;
         }
 
-        public override ViewResult View()
-        {
-            ViewResult result = base.View();
+        private ViewResult SetCookies(ViewResult result)
+		{
+			result.ViewData["requestCount"] = Request.Cookies["requestCount"] ?? "0";
+			result.ViewData["useCookies"] = Request.Cookies["gdpr"] ?? "";
             
-            result.ViewData["requestCount"] = Request.Cookies["requestCount"] ?? "0";
-            result.ViewData["useCookies"] = Request.Cookies["gdpr"] ?? "";
             return result;
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public override ViewResult View()
+        {
+            ViewResult result = SetCookies(base.View());
+            return result;
+        }
+
+		public override ViewResult View(string? viewName, object? model)
+		{
+			ViewResult result = SetCookies(base.View(viewName, model));
+			return result;
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
