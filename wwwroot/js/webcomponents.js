1010599e57bbb97acd771f86690d7441830aed37
Maarten van Keulen <mcmaartenz@outlook.com>
2023-06-28 00:12:35 +0200

    Chat page

---
+++ wwwroot/js/webcomponents.js
@@ -0,0 +1,23 @@
+﻿class Template extends HTMLElement {
+    shadowRoot;
+
+    constructor() {
+        super();
+        this.shadowRoot = this.attachShadow({ mode: 'open' });
+    }
+
+    applyTemplate(id) {
+        const template = document.getElementById(id);
+        let clone = template.content.cloneNode(true);
+        this.shadowRoot.appendChild(clone);
+    }
+}
+
+class ChatMessage extends Template {
+    constructor() {
+        super();
+        this.applyTemplate('chat-msg');
+    }
+}
+
+window.customElements.define('chat-msg', ChatMessage);
