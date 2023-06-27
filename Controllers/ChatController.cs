2b4dbcf085f92680a34e235ccb51537ef7a71e16
Maarten van Keulen <mcmaartenz@outlook.com>
2023-06-27 16:03:44 +0200

    Chat controller

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using ChatApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    [Authorize]
+    public class ChatController : BaseController
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ChatController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<HomeController> logger)
+            : base(userManager, signInManager, logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
