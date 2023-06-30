d801b9ae41eac2c765b82273560672ca0210641f
Maarten van Keulen <mcmaartenz@outlook.com>
2023-06-30 09:55:43 +0200

    Insert voor nieuwe db

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,9 @@
-﻿use [ChatAppDatabase];
-select * from [Messages];
+﻿use [aspnet-ChatApp-85310d83-b140-4f88-bc02-ef5b14494bfe];
+select * from [AspNetRoles];
+select * from [AspNetUsers];
+select * from [AspNetUserRoles];
+
+
+insert into [AspNetUserRoles] values
+('ebb71759-1d7a-4b9c-9e14-82357c72d790',
+'b3c7c71d-44ac-4f8c-8b65-9c9513d9affc');
